99a383e389f017281e084b072fb0b514d5fadc72
aariana12 <90168628+aariana12@users.noreply.github.com>
2023-12-17 21:31:26 -0500

    initial board!

--- Board.py
+++ Board.py
@@ -27,8 +27,9 @@ class Board:
             print("|")
             print("+--+--+--+--+--+")
 
-    def setup_workers(self, workers):
-        if workers == "['A', 'B']":
+    def setup_workers(self, color):
+        print("we made it!", color)
+        if color == "white":
             self.cells[3][1]['worker'] = ('A', 3, 1)
             self.cells[1][3]['worker'] = ('B', 1, 3)
             return [('A', 3, 1), ('B', 1, 3)]
@@ -36,5 +37,7 @@ class Board:
             self.cells[1][1]['worker'] = ('Y', 1, 1)
             self.cells[3][3]['worker'] = ('Z', 3, 3)
             return [('Y', 1, 1), ('Z', 3, 3)]  
+    
     def is_valid_direction(self, worker, direction):
+        pass
         
--- Game.py
+++ Game.py
@@ -1,6 +1,8 @@
 from Board import Board
 from copy import deepcopy
 from players.HumanPlayer import HumanPlayer
+# from players.HeuristicPlayer import HeuristicPlayer
+# from players.RandomPlayer import RandomPlayer
 
 class Santorini:
     """
@@ -11,12 +13,12 @@ class Santorini:
     def __init__(self, white_type, blue_type, undo_redo=False, score=False):
         self.board = Board()
         # pass in the type of player (human, heursitics, random)
-        self.white_type = white_type
-        self.blue_type = blue_type
+        # self.white_type = white_type
+        # self.blue_type = blue_type
         self.undo_redo_setting = undo_redo
         self.score_setting = score
-        self.players = [self.make_player(white_type, "white", ['A', 'B']), 
-                        self.make_player(blue_type, "blue", ['Y', 'Z'])] # an array of type Player
+        self.players = [self.make_player(white_type, ['A', 'B'], "white"), 
+                        self.make_player(blue_type, ['Y', 'Z'], "blue")] # an array of type Player
         self.turn_count = 1
         self.curr_player = 0 # start with white
         self.move_history = [] # save ALL MOVES
@@ -29,19 +31,15 @@ class Santorini:
 
     def make_player(self, player_type, color, workers):
         if player_type == 'human':
-            return HumanPlayer(color, workers, self.board)
-        # elif player_type == 'heuristic':
-        #     return Heuristic(name, workers, self.board)
-        # else:  # player_type == 'random'
-        #     return Random(name, workers, self.board)
+            return HumanPlayer(player_type, workers, color, self.board)
+        elif player_type == 'heuristic':
+            return HeuristicPlayer(player_type, workers, color, self.board)
+        else:  # player_type == 'random'
+            return RandomPlayer(player_type, workers, color, self.board)
 
     def make_moves(self):
-        # print(self.players[0].workers)
         for player in self.players:
-            print(player.workers)
-            print(player.player_type)
-            # print(player.name)
-            self.board.setup_workers(player.workers)
+            player.workers = self.board.setup_workers(player.color)
         self.board.display()
 
     def undo(self):
